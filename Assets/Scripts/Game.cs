e5570cef343d709b826fb1eabef2d3481653e4fc
pasdo501 <dominikpaschke@hotmail.com>
2019-09-27 11:37:42 +1200

    Initial Commit
    - Blank scene, start on Grid work

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public int[,] Grid
+    {
+        get { return grid; }
+        set { grid = value; }
+    }
+    private int[,] grid;
+    private int vertical;
+    private int horizontal;
+    private int cols;
+    private int rows;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vertical = (int) Camera.main.orthographicSize;
+        horizontal = vertical * (Screen.width / Screen.height);
+
+        cols = horizontal * 2;
+        rows = vertical * 2;
+
+        Grid = new int[rows, cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Grid[i, j] = Random.Range(0, 2);
+            }
+        }
+        
+        Debug.Log("Done");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
